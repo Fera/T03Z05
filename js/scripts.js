f6e5facc59292877ee04cc91fc60fdf6d00e7546
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-04-16 22:27:36 +0200

    Poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,24 @@
 (function() {
 
-if(!window.FileReader) return;
+    if(!window.FileReader) return;
 
-var fileInp = $("#converterInput");
+    var fileInput = $("#converterInput");
 
-fileInp.change(function() {
-    var files = this.files;
-    var file = files[0];
-    reader = new FileReader();
-    reader.readAsBinaryString(file);
-    
-    reader.onload = function(e) {
-        var converter = new showdown.Converter();
-        var toConvert = reader.result;
-        var converted = converter.makeHtml(toConvert);
-        $("textarea").html(converted);
-        $("#selectBtn").on("click", function() {
-            $("textarea").select();
-            document.execCommand("copy");
-        });
-    };
-
-});
-
-
+    fileInput.change(function() {
+        var files = this.files;
+        var file = files[0];
+        reader = new FileReader();
+        reader.readAsBinaryString(file);
+        
+        reader.onload = function(e) {
+            var converter = new showdown.Converter();
+            var toConvert = reader.result;
+            var converted = converter.makeHtml(toConvert);
+            $("textarea").html(converted);
+            $("#selectBtn").on("click", function() {
+                $("textarea").select();
+                document.execCommand("copy");
+            });
+        };
+    });
 })();
